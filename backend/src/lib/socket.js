0ac819a34d910145c967e362570aa99b1b30a047
Kenikool <114859959+Kenikool@users.noreply.github.com>
2025-05-05 09:46:31 +0100

    fronend_backend_connection_to_socket.io

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,30 +7,34 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "http://localhost:5174"],
   },
 });
 
-export function getReceiverSocketId(userId) {
+/* export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
 // used to store online users
 const userSocketMap = {}; // {userId: socketId}
 
+// listening to events connect
+ */
+// connecting to the socket
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
-  const userId = socket.handshake.query.userId;
+  /* const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
   // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
+ */
+  // disconnecting from the socket
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
-    delete userSocketMap[userId];
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    // delete userSocketMap[userId];
+    // io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
 
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,19 +1,23 @@
 import { create } from "zustand";
 import axiosInstance from "../lib/axios";
 import toast from "react-hot-toast";
-
-const useAuthStore = create((set) => ({
+import { io } from "socket.io-client";
+// FOR SOCKET
+const BASE_URL = "http://localhost:5000";
+const useAuthStore = create((set, get) => ({
   authUser: null,
   isSigningUp: false,
   isLoggingIn: false,
   isUpdatingProfile: false,
   isCheckingAuth: true,
   onlineUsers: [],
+  socket: null,
 
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get("/auth/check");
       set({ authUser: res.data });
+      get().connectSocket();
     } catch (error) {
       set({ authUser: null });
       console.log("error in checkAuth: ", error.message);
@@ -27,6 +31,7 @@ const useAuthStore = create((set) => ({
       const res = await axiosInstance.post("/auth/signup", data);
       set({ authUser: res.data });
       toast.success("Account created successfully");
+      get().connectSocket();
     } catch (error) {
       console.log("error in signUp: ", error.message);
       toast.error("Failed to create account", error.response.data.message);
@@ -40,6 +45,8 @@ const useAuthStore = create((set) => ({
       const res = await axiosInstance.post("/auth/login", data);
       set({ authUser: res.data });
       toast.success("Logged in successfully");
+      // after login we want to connect to socket
+      get().connectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
     } finally {
@@ -52,6 +59,7 @@ const useAuthStore = create((set) => ({
       await axiosInstance.post("/auth/logout");
       set({ authUser: null });
       toast.success("Logged out successfully");
+      get().disconnectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
     }
@@ -62,12 +70,23 @@ const useAuthStore = create((set) => ({
       const res = await axiosInstance.put("/auth/update-profile", data);
       set({ authUser: res.data });
       toast.success("Profile updated successfully");
+      get().connectSocket();
     } catch (error) {
       console.log("error in update profile:", error);
       toast.error(error.response.data.message);
     } finally {
       set({ isUpdatingProfile: false });
     }
+  },
+  connectSocket: () => {
+    const { authUser } = get();
+    if (!authUser || get().socket?.connected) return;
+    const socket = io(BASE_URL);
+    socket.connect();
+    set({ socket: socket });
+  },
+  disconnectSocket: () => {
+    if (get().socket?.connected) get().socket.disconnect();
   },
 }));
 
